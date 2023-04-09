b37ef94ab4a4b5f6eb7cf08b1a5eb47286167194
Cristian-C69 <cristian.gula69@gmail.com>
2023-04-09 19:58:25 -0300

    uno

---
+++ proyecto-prueba/src/paquetedeprueba/Ejercicio1.java
@@ -0,0 +1,19 @@
+
+package paquetedeprueba;
+
+import java.util.Scanner;
+
+
+public class Ejercicio1 {
+    
+    public static void main(String[] args) {
+       
+        
+        
+        
+    }
+    
+    
+    
+    
+}
